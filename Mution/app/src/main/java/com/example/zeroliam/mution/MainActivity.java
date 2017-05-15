5767512f3ee0cd2e9215dbf0948b57f62d7f4c50
ZeroLiam <liselotr@gmail.com>
2017-05-15 13:10:20 +0200

    Mution - added graph library

--- Mution/app/src/main/java/com/example/zeroliam/mution/MainActivity.java
+++ Mution/app/src/main/java/com/example/zeroliam/mution/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.zeroliam.mution;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,13 +13,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void goToSensor(){
-        Intent sensorIntent = new Intent(MainActivity.this, SensorActivity.class);
+    public void goToSensor(View v){
+        Intent sensorIntent = new Intent(getApplicationContext(), SensorActivity.class);
         startActivity(sensorIntent);
     }
 
-    public void goToPlayer(){
-        Intent sensorIntent = new Intent(MainActivity.this, PlayerActivity.class);
+    public void goToPlayer(View v){
+        Intent sensorIntent = new Intent(getApplicationContext(), PlayerActivity.class);
         startActivity(sensorIntent);
     }
 }
